56d013edbcef3f8aca62a17795dda96756e810f1
Flavio_Rodrigues <flahrodrigues@yahoo.com.br>
2025-07-10 14:07:56 +0000

    adicionando pages

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the home page of our application.</p>
+      <p>Nova versão da página em tempo real</p>
+    </div>
+  );
+}
+
+export default Home;
