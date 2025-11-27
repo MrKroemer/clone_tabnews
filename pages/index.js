3776bf387d525938d64f0e0b4874f3869968c486
MrKroemer <flahrodrigues@yahoo.com.br>
2025-11-27 15:13:19 -0300

    Add  file

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1>Seja bem vindoa nossa Home Page</h1>
+      <h1>Seja bem vindo a nossa Home Page</h1>
       <p>Esta é a página principal da aplicação.</p>
       <p>Nova versão da página em tempo real</p>
     </div>
